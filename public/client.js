e32b32b0b4720ccec0b9728cacdfa26b68f2fdea
Glitch (hello-express) <none>
2018-12-04 06:42:51 +0000

    💘🐔 Checkpoint ./server.js:347658/1791 ./package.json:347658/52 ./public/index.html:347658/2966 ./public/style.css:347658/1755 ./public/client.js:347658/2169

--- public/client.js
+++ public/client.js
@@ -1,42 +1,21 @@
-// client-side js
-// run by the browser each time your view template is loaded
+const apiUrl = 'https://api.thecatapi.com/v1/images/search?limit=50';
+const tpl = document.querySelector('template').content;
+const container = document.querySelector('ul');
 
-console.log('hello world :o');
-
-// our default array of dreams
-const dreams = [
-  'Find and count some sheep',
-  'Climb a really tall mountain',
-  'Wash the dishes'
-];
-
-// define variables that reference elements on our page
-const dreamsList = document.getElementById('dreams');
-const dreamsForm = document.forms[0];
-const dreamInput = dreamsForm.elements['dream'];
-
-// a helper function that creates a list item for a given dream
-const appendNewDream = function(dream) {
-  const newListItem = document.createElement('li');
-  newListItem.innerHTML = dream;
-  dreamsList.appendChild(newListItem);
+function init () {
+  fetch(apiUrl)
+  .then(response => response.json())
+  .then(cats => {
+    container.innerHTML = '';
+    cats
+      .map(cat => {
+        const li = document.importNode(tpl, true);
+        li.querySelector('img').src = cat.url;
+        return li;
+      }).forEach(li => container.appendChild(li));
+  })
 }
 
-// iterate through every dream and add it to our page
-dreams.forEach( function(dream) {
-  appendNewDream(dream);
-});
+init();
 
-// listen for the form to be submitted and add a new dream when it is
-dreamsForm.onsubmit = function(event) {
-  // stop our form submission from refreshing the page
-  event.preventDefault();
-
-  // get dream value and add it to the list
-  dreams.push(dreamInput.value);
-  appendNewDream(dreamInput.value);
-
-  // reset form 
-  dreamInput.value = '';
-  dreamInput.focus();
-};
+document.querySelector('button').addEventListener('click', init);
--- server.js
+++ server.js
@@ -4,19 +4,31 @@
 // init project
 const express = require('express');
 const app = express();
+const rendertron = require('rendertron-middleware');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const PORT = process.env.PORT || 8080;
+const DIST_FOLDER = process.cwd() + '/public';
+// Add googlebot to the list of bots we will use Rendertron for
+const BOTS = rendertron.botUserAgents.concat('googlebot');
+const BOT_UA_PATTERN = new RegExp(BOTS.join('|'), 'i');
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.set('view engine', 'html');
+// Add Rendertron middleware to send bot requests to Rendertron
+app.use(rendertron.makeMiddleware({
+  proxyUrl: 'https://render-tron.appspot.com/render', // this is a DEMO URL! Do not use this in production!
+  userAgentPattern: BOT_UA_PATTERN
+}));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+// Static Assets
+app.get('*.*', express.static('public'));
+
+// Point all routes to index...
+app.get('*', (req, res) => {
+  res.set('Vary', 'User-Agent');
+  res.sendFile(DIST_FOLDER + '/index.html');
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+// Start Express Server
+app.listen(PORT, () => {
+  console.log(`Node Express server listening on ${PORT}`);
 });
